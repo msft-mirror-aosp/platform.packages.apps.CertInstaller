11fe54dee8a8e2d6e395b697b12a93b658a41b2f
Chiachang Wang <chiachangwang@google.com>
2020-08-03 20:37:13 +0800

    Update language to comply with Android's inclusive language guidance
    See https://source.android.com/setup/contribute/respectful-code
    for reference.
    
    Bug: 161896447
    Test: m
    Change-Id: Ideeb63461656f886d78225e5b5c66ad17a04ef14

--- src/com/android/certinstaller/CertInstaller.java
+++ src/com/android/certinstaller/CertInstaller.java
@@ -247,7 +247,7 @@ public class CertInstaller extends Activity {
     }
 
     private void installOthers() {
-        // Sanity check: Check that there's either:
+        // Check that there's either:
         // * A private key AND a user certificate, or
         // * A CA cert.
         boolean hasPrivateKeyAndUserCertificate =
