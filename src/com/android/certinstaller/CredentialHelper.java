c9e9ac7154eeccdba56301fe9300554b2d3e2d98
Daulet Zhanguzin <dauletz@google.com>
2021-01-25 20:20:51 +0000

    CertInstaller: use unbundled version of BouncyCastle. BouncyCastle APIs cannot be exposed in updatable ART.
    Bug: 154796679
    Test: Treehugger
    Test: atest CertInstallerRoboTests
    
    Change-Id: I81b43f7dd7b04d20a434ea80867b09c2cecf1eca

--- src/com/android/certinstaller/CredentialHelper.java
+++ src/com/android/certinstaller/CredentialHelper.java
@@ -36,11 +36,11 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
-import com.android.org.bouncycastle.asn1.ASN1InputStream;
-import com.android.org.bouncycastle.asn1.ASN1Sequence;
-import com.android.org.bouncycastle.asn1.DEROctetString;
-import com.android.org.bouncycastle.asn1.x509.BasicConstraints;
 import com.android.org.conscrypt.TrustedCertificateStore;
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.DEROctetString;
+import org.bouncycastle.asn1.x509.BasicConstraints;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
